6506492b9231b65a078b7efd93587e7a5c22e116
NeguliSimanis <simanism@inbox.lv>
2020-01-31 23:25:18 +0200

    added custom fonts, item prefab

---
+++ Assets/Scripts/GodController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodController : MonoBehaviour
+{
+   
+
+
+    public void GenerateQuest()
+    {
+        
+    }
+
+    public void DisplayQuestItems()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Skull,
+    Boot,
+    Knife
+}
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private ItemType itemType;
+    [SerializeField]
+    private Sprite itemSprite;
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            PickupItem();
+        }
+    }
+
+    private void PickupItem()
+    {
+        Destroy(gameObject);
+    }
+
+}
