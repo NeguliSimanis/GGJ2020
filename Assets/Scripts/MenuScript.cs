eb977e4acdd8e1e0cfde9c4383b15e257010c5cc
KasixLv <kasixlv@gmail.com>
2020-01-31 20:03:45 +0200

    Main menu and folder setup

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onPlay()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void onOptions()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void onExit()
+    {
+        Application.Quit();
+    }
+}
