54ecd8d8b95fe0ba9ab422f6796f13c7022048e1
Martin <Martinviidik@gmail.com>
2020-02-01 00:58:18 +0000

    super advanced enemy ai

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float radius = 20;
+
+    IAstarAI ai;
+
+    void Start () 
+    {
+        ai = GetComponent<IAstarAI>();
+    }
+
+    Vector3 PickRandomPoint () 
+    {
+        var point = Random.insideUnitSphere * radius;
+
+        point += ai.position;
+        Debug.Log(point);
+        return point;
+    }
+
+    void Update () {
+        if (!ai.pathPending && (ai.reachedEndOfPath || !ai.hasPath))
+        {
+            ai.destination = PickRandomPoint();
+            ai.SearchPath();
+        }
+    }
+}
