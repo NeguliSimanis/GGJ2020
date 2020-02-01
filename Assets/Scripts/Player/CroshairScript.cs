2e1d7f750c029ebf0c0b1afd1394c698d43b389f
KasixLv <kasixlv@gmail.com>
2020-02-01 12:14:09 +0200

    Bunch of stufff

---
+++ Assets/Scripts/Player/CroshairScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CroshairScript : MonoBehaviour
+{
+    private Vector3 mouseCoordinates;
+    public float mouseSensitivity = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseCoordinates = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.transform.position = new Vector3(mouseCoordinates.x, mouseCoordinates.y, -1f);
+
+    }
+}
